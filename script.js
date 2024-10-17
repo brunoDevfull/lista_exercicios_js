7df9926b9545b805993874fde49cf582fcba0cb1
Bruno Ribeiro <brunoribeiro.cgms@gmail.com>
2024-10-16 21:34:54 -0400

    Arquivos editados index.html e script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// 1. Faça um algoritmo que receba dois números e exiba o resultado da sua soma.
+
 function somar() {
     var numero1 = document.getElementById('numero1').value;
     var numero2 = document.getElementById('numero2').value;
@@ -11,9 +11,6 @@ function somar() {
     
 }
 
-
-// 2. Faça um algoritmo que receba dois números e ao final mostre a soma, subtração,
-// multiplicação e a divisão dos números lidos.
 function calculadora(){
 
     var valor1 = document.getElementById('valor1').value;
@@ -34,17 +31,19 @@ function calculadora(){
     var divisao = valor1 / valor2;
     document.getElementById('dividir').textContent = `A Divisão é: ${divisao}`;
 
-// console.log(`A Soma de ${valor1} + ${valor2} é: ${adicao}`);
-// console.log(`A Subtração de ${valor1} - ${valor2} é: ${subtracao}`);
-// console.log(`A Multiplicção de ${valor1} * ${valor2} é: ${multiplicacao}`);
-// console.log(`A Divisão de ${valor1} / ${valor2} é: ${divisao}`);
 }
-// 3. Escrever um algoritmo para determinar o consumo médio de um automóvel sendo
-// fornecida a distância total percorrida pelo automóvel e o total de combustível gasto.
-var distancia = 100;
-var combustivelGasto = 50;
-var consumoMedio = distancia / combustivelGasto;
-console.log(`O consumo médio do Veiculo é: ${consumoMedio} litros por km`);
+
+function cosumoMedio(){
+    var distancia = document.getElementById("distancia").value;
+    var combustivelGasto = document.getElementById("combustivel_gasto").value;
+
+    distancia = parseInt(distancia);
+    combustivelGasto = parseInt(combustivelGasto);
+
+    var consumoMedio = distancia / combustivelGasto;
+    document.getElementById('dividir').textContent = `O Consumo medio é: ${cosumoMedio}`;
+}
+    
 
 // 4. Escrever um algoritmo que leia o nome de um vendedor, o seu salário fixo e o total de
 // vendas efetuadas por ele no mês (em dinheiro). Sabendo que este vendedor ganha 15%
